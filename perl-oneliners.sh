81ebd12e320094b3b912a0a059b735516a28db6d
Janos Gyerik <janos.gyerik@gmail.com>
2013-05-01 23:27:24 +0300

    Create perl-oneliners.sh

---
+++ perl-oneliners.sh
@@ -0,0 +1,8 @@
+# urlencode
+echo some_url | perl -MURI::Escape -ne 'chop; print uri_escape($_), "\n"'
+
+# base64 encode
+perl -MMIME::Base64 -e '$a = join("", <>); print encode_base64($a)' < input
+
+# base64 decode
+perl -MMIME::Base64 -e '$a = join("", <>); print decode_base64($a)' < input
